2e4eae09413a7b2d6f5df82107c3af55437b697a
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-04-10 08:45:46 +0200

    show current encoding task and it's rank

---
+++ bbb-current-encoding.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -e
+
+# show current encoding task and it's rank
+
+#id=$( ps ax | grep ffmpeg | grep presentation | sed -e 's,^.*presentation/,,' -e 's,/.*$,,' )
+id=$( ps ax | grep process/presentation.rb | grep ruby | sed 's/^.*-m //' )
+if [ ! -z "$id" ] ; then
+	echo "# $(date) $id"
+	bbb-record --list | cat -n | head -200 | grep $id && exit 0
+fi
