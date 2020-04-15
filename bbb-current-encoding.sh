6f90cae549923be99d5ba86fac5262a8351e4138
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-04-15 10:03:50 +0200

    added /proc/loadavg

--- bbb-current-encoding.sh
+++ bbb-current-encoding.sh
@@ -5,6 +5,6 @@
 #id=$( ps ax | grep ffmpeg | grep presentation | sed -e 's,^.*presentation/,,' -e 's,/.*$,,' )
 id=$( ps ax | grep process/presentation.rb | grep ruby | sed 's/^.*-m //' )
 if [ ! -z "$id" ] ; then
-	echo "# $(date) $id"
+	echo "# $(date) $id $(cat /proc/loadavg)"
 	bbb-record --list | cat -n | head -200 | grep $id && exit 0
 fi
